4401be3087123cb36e12cc8ca110d647d9e6cd88
Jokūbas Rybelis <frizjrs@protonmail.com>
2023-03-02 19:00:36 +0200

    Add ExchangeRate model and dto. Introduce attribute validation with a custom max date validator.

---
+++ ExchangeRatePreviewer.Core/Models/Dtos/ExchangeRate/ExchangeRateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ExchangeRatePreviewer.Core.Validation.DataAnnotations;
+
+namespace ExchangeRatePreviewer.Core.Models.Dtos.ExchangeRate;
+
+public class ExchangeRateDto
+{
+    [Required, MinLength(3), MaxLength(3)] public string Currency { get; set; } = default!;
+    
+    [Required, DataType(DataType.Date), CustomDateValidation] public DateTime Date { get; set; }
+    
+    [Required] public int Quantity { get; set; }
+
+    [Required] public string UnitDescription { get; set; } = default!;
+    
+    [Required] public decimal Rate { get; set; }
+}
---
+++ ExchangeRatePreviewer.Core/Validation/DataAnnotations/CustomDateValidation.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExchangeRatePreviewer.Core.Validation.DataAnnotations;
+
+public class CustomDateValidation : ValidationAttribute
+{
+    private const string MAXIMUM_ACCEPTED_QUERY_DATE =
+        "This service only accepts requests for the period of up to the 31st of December, 2014.";
+
+    private readonly DateTime _maxAcceptedDate = new DateTime(2014 - 12 - 31).Date;
+
+    /// <summary>
+    /// Checks whether the provided date is prior to the start of the year 2015.
+    /// </summary>
+    /// <param name="value">Input from the calling method</param>
+    /// <param name="validationContext">Context for validation check</param>
+    /// <returns>Boolean success or error and an error message, if any.</returns>
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var valueString = value?.ToString();
+
+        if (string.IsNullOrWhiteSpace(valueString))
+            // No value, so return success.
+            return ValidationResult.Success;
+
+        if (!DateTime.TryParse(valueString, out var date))
+            // Not a valid date, return error.
+            return new ValidationResult("Unable to convert the date provided to a valid date.");
+
+        return date > _maxAcceptedDate ?
+            // Above maximum accepted date, return error.
+            new ValidationResult(MAXIMUM_ACCEPTED_QUERY_DATE) :
+            // Return success
+            ValidationResult.Success;
+    }
+}
