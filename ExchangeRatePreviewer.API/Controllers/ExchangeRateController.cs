8b2c1a7b95036109a2d29b670ae672f6ab860ca2
Jokūbas Rybelis <frizjrs@protonmail.com>
2023-03-04 22:20:31 +0200

    Add exchange rate controller and service logic

---
+++ ExchangeRatePreviewer.API/Controllers/ExchangeRateController.cs
@@ -0,0 +1,47 @@
+﻿using ExchangeRatePreviewer.Core.Interfaces;
+using ExchangeRatePreviewer.Core.Interfaces.Services;
+using ExchangeRatePreviewer.Core.Models.Dtos.ExchangeRate;
+using ExchangeRatePreviewer.Core.Services.SOAP;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ExchangeRatePreviewer.API.Controllers;
+
+/// <summary>
+/// Handles API requests for LTL currency exchange rates from bank of Lithuania
+/// </summary>
+[Route("lb/exchangeRates")]
+public class ExchangeRateController : ControllerBase
+{
+    private readonly IExchangeRateService _exchangeRateService;
+    private readonly IBankOfLithuaniaSoapClientSettings _clientSettings;
+
+    //private readonly IOptions<BankOfLithuaniaSoapClientSettings> _bankOfLithuaniaSoapClientSettings;
+    public ExchangeRateController(IExchangeRateService exchangeRateService,
+        IBankOfLithuaniaSoapClientSettings
+            clientSettings /*, IOptions<BankOfLithuaniaSoapClientSettings> bankOfLithuaniaSoapClientSettings*/)
+    {
+        _exchangeRateService = exchangeRateService;
+        _clientSettings = clientSettings;
+        //_bankOfLithuaniaSoapClientSettings = bankOfLithuaniaSoapClientSettings;
+    }
+
+    [HttpGet, /*ValidateFluent*/]
+    public async Task<ActionResult<List<ExchangeRateDto>>> GetAll(DateTime date)
+    {
+        /*var bankOfLithuaniaSoapClientSettings = new BankOfLithuaniaSoapClientSettings
+        {
+            BaseUrl = _clientSettings.BaseUrl,
+            RequestUrl = _clientSettings.RequestUrl,
+            TimeoutMs = _clientSettings.TimeoutMs,
+            ResultXmlNamespace = _clientSettings.ResultXmlNamespace
+        };*/
+
+        var result = await _exchangeRateService.GetAllExchangeRatesByDate(date/*, bankOfLithuaniaSoapClientSettings*/);
+        
+        if (result is null)
+            return NotFound();
+
+        return result;
+    }
+}
---
+++ ExchangeRatePreviewer.Core/Services/ExchangeRateService.cs
@@ -0,0 +1,58 @@
+﻿using ExchangeRatePreviewer.Core.Interfaces;
+using ExchangeRatePreviewer.Core.Interfaces.Services;
+using ExchangeRatePreviewer.Core.Interfaces.Services.Validators;
+using ExchangeRatePreviewer.Core.Models.Dtos.ExchangeRate;
+using ExchangeRatePreviewer.Core.Services.SOAP;
+
+namespace ExchangeRatePreviewer.Core.Services;
+
+public class ExchangeRateService : IExchangeRateService
+{
+    private readonly IDateValidation _dateValidation;
+    private readonly IBankOfLithuaniaSoapClient _client;
+
+    public ExchangeRateService(IDateValidation dateValidation, IBankOfLithuaniaSoapClient client)
+    {
+        _dateValidation = dateValidation;
+        _client = client;
+    }
+
+    public async Task<List<ExchangeRateDto>?> GetAllExchangeRatesByDate(DateTime date/*,
+        BankOfLithuaniaSoapClientSettings clientSettings*/)
+    {
+        _dateValidation.IsDateValid(date);
+        
+        //var client = new BankOfLithuaniaSoapClient(clientSettings); 
+        
+        var selectedDateExchangeRates = await _client.GetExchangeRatesByDate(date);
+        if (selectedDateExchangeRates is null)
+            return null;
+        
+        var previousDayExchangeRates = await _client.GetExchangeRatesByDate(date.AddDays(-1));
+        if (previousDayExchangeRates is null)
+            return null;
+
+        var exchangeRatesWithRateChange = GetExchangeRatesChangeOverPreviousDay(
+            selectedDateExchangeRates.ExchangeRateDtos,
+            previousDayExchangeRates.ExchangeRateDtos);
+        var sortedExchangeRatesList = exchangeRatesWithRateChange.OrderByDescending(xrd => xrd.RateChangeVsPreviousDay).ToList();
+
+        return sortedExchangeRatesList;
+    }
+
+    private static IEnumerable<ExchangeRateDto> GetExchangeRatesChangeOverPreviousDay(List<ExchangeRateDto> selectedDateExchangeRates, List<ExchangeRateDto> previousDayExchangeRates)
+    {
+        foreach (var exchangeRateDto in selectedDateExchangeRates)
+        {
+            var matchingExchangeRate =
+                previousDayExchangeRates.FirstOrDefault(erd => erd.Currency == exchangeRateDto.Currency);
+
+            if (matchingExchangeRate is not null)
+                exchangeRateDto.RateChangeVsPreviousDay = exchangeRateDto.Rate - matchingExchangeRate.Rate;
+            else
+                exchangeRateDto.RateChangeVsPreviousDay = 0M;
+        }
+
+        return selectedDateExchangeRates;
+    }
+}
